1e4233d5c3b0ceb605244a8d391960fb1ab9ce6a
diart <diart@DESKTOP-GRU8VQP>
2025-12-04 00:25:43 +0100

    Codestyle fix

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -12,21 +12,21 @@ public class WordleDictionaryLoader {
         this.pr = pr;
     }
 
-    public WordleDictionary load(String Load) throws DictionaryLoadException {
+    public WordleDictionary load(String load) throws DictionaryLoadException {
         ArrayList<String> words = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(new FileInputStream(Load), StandardCharsets.UTF_8))) {
+                new InputStreamReader(new FileInputStream(load), StandardCharsets.UTF_8))) {
             String line;
             while ((line = br.readLine()) != null) {
                 words.add(line);
             }
 
         } catch (IOException e) {
-            pr.println("Error loading dictionary from file " + Load + ": " + e.getMessage());
+            pr.println("Error loading dictionary from file " + load + ": " + e.getMessage());
             throw new DictionaryLoadException("Failed to load dictionary", e);
         }
         if (words.isEmpty()) {
-            throw new DictionaryLoadException("Dictionary is empty: " + Load);
+            throw new DictionaryLoadException("Dictionary is empty: " + load);
         }
         WordleDictionary dict = new WordleDictionary();
         dict.init(words);
