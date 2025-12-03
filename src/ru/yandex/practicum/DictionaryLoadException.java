adb305d3454db15d77ff99fdd41df6020865ae2f
diart <diart@DESKTOP-GRU8VQP>
2025-12-04 00:19:04 +0100

    Codestyle fix

--- src/ru/yandex/practicum/DictionaryLoadException.java
+++ src/ru/yandex/practicum/DictionaryLoadException.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum;
 
 public class DictionaryLoadException extends Throwable {
-    public DictionaryLoadException(String ex){
+    public DictionaryLoadException(String ex) {
         super(ex);
     }
-    public DictionaryLoadException(String ex, Throwable clause){
+    public DictionaryLoadException(String ex, Throwable clause) {
         super(ex, clause);
     }
 }
